1ef3d8220169fd725e9cfbc586944180ca5a064e
Gleb <monachusgl@gmail.com>
2022-06-23 22:13:27 +0300

    Заменены все request на кастомные.

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,13 +1,14 @@
 import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         # REGISTER
         registration_data = self.prepare_registration_data()
-        response1 = requests.post("https://playground.learnqa.ru/api/user/", data=registration_data)
+        response1 = MyRequests.post("/user/", data=registration_data)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.assert_json_has_key(response1, "id")
@@ -22,7 +23,8 @@ class TestUserEdit(BaseCase):
             'email': email,
             'password': password
         }
-        response2 = requests.post("https://playground.learnqa.ru/api/user/login", data=login_data)
+
+        response2 = MyRequests.post("/user/login", data=login_data)
 
         auth_sid = self.get_cookie(response2, "auth_sid")
         token = self.get_header(response2, "x-csrf-token")
@@ -30,8 +32,8 @@ class TestUserEdit(BaseCase):
         # EDIT
         new_name = "Changed name"
 
-        response3 = requests.put(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
             cookies={'auth_sid': auth_sid},
             headers={'x-csrf-token': token},
             data={'firstName': new_name}
@@ -40,8 +42,8 @@ class TestUserEdit(BaseCase):
         Assertions.assert_code_status(response3, 200)
 
         # GET
-        response4 = requests.get(
-            f"https://playground.learnqa.ru/api/user/{user_id}",
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
             cookies={'auth_sid': auth_sid},
             headers={'x-csrf-token': token}
         )
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,14 +1,13 @@
-import datetime
-import requests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+from lib.my_requests import MyRequests
 
 
 class TestUserRegister(BaseCase):
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
@@ -17,7 +16,7 @@ class TestUserRegister(BaseCase):
         email = 'vinkotov@examples.com'
         data = self.prepare_registration_data(email)
 
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        response = MyRequests.post("/user/", data=data)
 
         Assertions.assert_code_status(response, 400)
 
