e7c6710ff4e3484b03aadf22a5bb203784b428ef
Gleb <monachusgl@gmail.com>
2022-06-23 21:36:12 +0300

    Добавлен тест на создание/изменение пользователя.

--- lib/assertions.py
+++ lib/assertions.py
@@ -41,6 +41,7 @@ class Assertions:
 
         assert name not in response_as_dict, f"Response JSON shouldn't have key {name}. But it's present"
 
+    @staticmethod
     def assert_json_has_not_keys(response: Response, names: list):
         try:
             response_as_dict = response.json()
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,41 +1,26 @@
 import datetime
 import requests
-from datetime import datetime
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
-class TestUserRegister(BaseCase):
-    def setup(self):
-        base_part = "learnqa"
-        domain = "examples.com"
-        random_part = datetime.now().strftime("%m%d%Y_%H%M%S")
-        self.email = f"{base_part}{random_part}@{domain}"
 
+class TestUserRegister(BaseCase):
     def test_create_user_successfully(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@examples.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email)
 
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
         Assertions.assert_code_status(response, 400)
+
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
             f"Unexpected response content {response.content}"
 
